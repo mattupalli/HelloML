d5f217f9fb276458f7e0765a1299838b64cd5d57
lastjedi <nmattupalli@gmail.com>
2019-08-19 15:03:07 +0200

    initial commit

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    #return render_template('index.html')
+    return '<h1>Deployed</h1>'
 
-@app.route('/', methods=['POST'])
-def getVal():
-    name= request.form['name']
+#@app.route('/', methods=['POST'])
+#def getVal():
+ #   return '<h1>Deployed</h1>'
+    #name= request.form['name']
     #converts string into int and multiplies by 2
-    n= (int(name))*2
+    #n= (int(name))*2
     #converts the multiples number into string back beacause this fuction apparently can only retrun a string
-    return str(n)
+    #return str(n)
     #return render_template('pass.html', n=name)
 
-if __name__ == '__main__':
-    app.Run()
+#if __name__ == '__main__':
+#   app.Run(debug=True)
